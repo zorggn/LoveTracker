0ace5559a236e53aedb99cd3423b7a53d6feb78a
zorg <zorg@atw.hu>
2017-03-17 13:30:26 +0100

    Update util.lua
    Renamed functions from ANSI to bin(ary);
    Implemented bin2flags.
    Fix references to these functions in other files.

--- util.lua
+++ util.lua
@@ -2,44 +2,69 @@
 -- by zorg @ 2016 § ISC
 
 local utf8 = require 'utf8'
-local bit = require 'bit'
+local bit  = require 'bit'
 
 local util = {}
 
-function util.str2hex(s)
+function util.str2hex(str, div)
 	local t = {}
-	for i=1,#s do table.insert(t, ('%X'):format(s:sub(i,i):byte())) end
-	return table.concat(t, ' ')
+	for i=1, #str do
+		table.insert(t, ('%X'):format(str:sub(i, i):byte()))
+	end
+	return table.concat(t, div)
 end
 
-function util.hex2str(h)
+function util.hex2str(hex)
 	local t = {}
-	for x in h:gmatch('%w+') do table.insert(t, string.char(tonumber(x,16))) end
+	for x in hex:gmatch('%w+') do
+		table.insert(t, string.char(tonumber(x, 16)))
+	end
 	return table.concat(t)
 end
 
-function util.ansi2utf8(s)
-	local t,m = {},0
-	for c in s:gmatch'.' do
-		local n = string.byte(c)
+function util.bin2utf8(str)
+	local t, m = {}, 0
+	for chr in str:gmatch'.' do
+		local n = string.byte(chr)
 		m = m + 1
 		t[m] = utf8.char(n)
 	end
 	return table.concat(t)
 end
 
-function util.ansi2number(s, endianness) -- 'BE' is default -- rename to bin2number
-	local n,m = 0, #s
-	if endianness == 'LE' then
-		for i = 1, m, 1 do n = n + s:sub(i,i):byte() * (256^(i-1)) end
-	else--[[if endianness == 'BE' then--]]
-		for i = 1, m, 1 do n = n + s:sub(i,i):byte() * (256^((m-1)-(i-1))) end
+function util.bin2num(str, ord, wsize)
+	wsize = wsize or 1
+	local n,m = 0, #str
+	if ord == 'LE' then
+		for i = 1, m, wsize do
+			for j = wsize-1, 0, -1 do
+				n = n + str:sub(i+j,i+j):byte() * (256^(i-1+(wsize-1-j)))
+			end
+		end
+	else --[[if ord == 'BE' then--]]
+		for i = 1, m, 1 do
+			n = n + str:sub(i,i):byte() * (256^((m-1)-(i-1)))
+		end
 	end
 	return n
 end
 
-function util.bin2bitfield(s)
-
+function util.bin2flags(str, ord)
+local t, n = {}, 0
+	while n < #str do
+		local c = str:sub(n+1,n+1):byte()
+		local i = 0
+		while i < 8 do
+			if ord == 'LE' then
+				t[(n*8)+i+1] = bit.band(c, 2^(7-i)) ~= 0 and true or false
+			else --[[if ord == 'BE' then--]]
+				t[(n*8)+i+1] = bit.band(c, 2^i) ~= 0 and true or false
+			end
+			i = i + 1
+		end
+		n = n + 1
+	end
+	return t
 end
 
 -----------
