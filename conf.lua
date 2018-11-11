a699ec7293565d602be7424e7d4d6651dd8152c1
zorggn <zorg@atw.hu>
2018-11-11 13:05:07 +0100

    updated löve version

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 ﻿function love.conf(t)
     t.identity = nil                    -- The name of the save directory (string)
-    t.version = "0.11.0"                -- The LÖVE version this game was made for (string)
+    t.version = "11.1"                -- The LÖVE version this game was made for (string)
     t.console = true                   -- Attach a console (boolean, Windows only)
     t.accelerometerjoystick = true      -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
     t.externalstorage = false           -- True to save files (and read from the save directory) in external storage on Android (boolean) 
